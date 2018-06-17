c7c7c7f1ba49d5be52850bf0d0796d2024d6e396
yatin-kapur <yatin.aol@gmail.com>
2018-06-17 01:01:21 -0400

    init

---
+++ add_game_entry.py
@@ -0,0 +1,33 @@
+import bs4 as bs
+import insert
+import urllib3
+
+def create_match_record(match_id):
+    url = 'http://www.football-lineups.com/match/%s' % match_id
+    user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'}
+    http = urllib3.PoolManager(2, user_agent)
+    response = http.request('GET', url)
+    soup = bs.BeautifulSoup(response.data, 'lxml')
+
+    teams = soup.find_all('span', {'id': 'titequip'})
+    teams = [t.text for t in teams]
+    home_team = teams[0]
+    away_team = teams[1]
+
+    goal_times = soup.find_all('td', {'width': '30', 'align': 'middle'})
+    goal_times = [int(gt.text[:-1]) for gt in goal_times]
+    score = soup.find_all('td', {'width': '25', 'align': 'middle'})
+    score = [s.text for s in score]
+
+    goals = [[goal_times[i], int(score[i][0]), int(score[i][-1])] for i in range(0, len(score)) if score[i] != ' ' and score[i] != '\xa0\xa0\xa0']
+    if goals == []:
+        raise ValueError("This match has not completed yet")
+
+    insert_dict = {'match_id': match_id, 'home_team': home_team, 'away_team': away_team}
+
+    for entry in goals:
+        insert_dict['minute'] = entry[0]
+        insert_dict['home_score'] = entry[1]
+        insert_dict['away_score'] = entry[2]
+        # call insert function
+        insert.insert('Scores', **insert_dict)
---
+++ insert.py
@@ -0,0 +1,41 @@
+import MySQLdb
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                     passwd="mysqlpass",
+                     db="leading_trailing")
+cursor = db.cursor()
+
+def insert(table_name, **kwargs):
+    template = "replace into %s (%s) values (%s);"
+    column_names = []
+    values = []
+    for k, v in kwargs.items():
+        column_names.append(k)
+        values.append(v)
+
+    column_string = ""
+    for n in column_names:
+        column_string += n + ","
+    column_string = column_string[:-1]
+
+    value_string = ""
+    for v in values:
+        if(v == None):
+            value = "NULL"
+        elif type(v) is int:
+            value = str(v)
+        elif type(v) is float:
+            value = str(v)
+        else:
+            try:
+                value = "'" + str(v) + "'"
+            except:
+                value = "'" + str(v.encode('utf-8')) + "'"
+        value_string += value + ","
+    value_string = value_string[:-1]
+    sql_insert = template % (table_name, column_string, value_string)
+    try:
+        cursor.execute(sql_insert)
+        db.commit()
+    except:
+        print("ERROR", sql_insert)
