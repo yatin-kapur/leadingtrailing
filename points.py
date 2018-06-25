78382c60e5a3deecdea65d61a1bc4400bf9bf50a
yatin-kapur <yatin.aol@gmail.com>
2018-06-25 17:55:34 -0400

    plotting up/down

--- points.py
+++ points.py
@@ -1,4 +1,5 @@
 import MySQLdb
+import pandas as pd
 import matplotlib.pyplot as plt
 #import insert
 
@@ -15,22 +16,16 @@ countries = cursor.fetchall()
 countries = [c[0] for c in countries] + [c[1] for c in countries]
 countries = list(set(countries))
 
-rounds_query = "select team, round_of_16 \
+rounds_query = "select team, groups, round_of_16 \
     from russia_stages;"
 cursor.execute(rounds_query)
-qualified = cursor.fetchall()
-qualified = {q[0]:q[1] for q in qualified}
-print(qualified)
+qualified_data = cursor.fetchall()
+qualified = {q[0]:q[2] for q in qualified_data}
+groups = {q[0]:q[1] for q in qualified_data}
 
 points = []
 leading_time = []
-
-#insert.insert(cursor, db, 'Russia_Stages', **{'team': country,
-#                                                'round_of_16': 0,
-#                                                'quarter_final': 0,
-#                                                'semi_final': 0,
-#                                                'final': 0,
-#                                                'winner': 0})
+trailing_time = []
 
 for country in countries:
     points_query = "select \
@@ -56,5 +51,35 @@ for country in countries:
     cursor.execute(lead_query)
     lead_time = int(cursor.fetchall()[0][0])
     leading_time.append(lead_time)
+
+    trail_query = "select  \
+        sum(if(((e.home_score < e.away_score and m.home_team = '%s') or (e.home_score > e.away_score and m.away_team = '%s')), 1, 0)) \
+        from extended_scores e \
+        join matches m on m.match_id = e.match_id;" % (country, country)
+
+    cursor.execute(trail_query)
+    trail_time = int(cursor.fetchall()[0][0])
+    trailing_time.append(trail_time)
+
+df_qual = [qualified[c] for c in countries]
+df_group = [groups[c] for c in countries]
+df = pd.DataFrame({'country': countries,
+                   'group': df_group,
+                   'leading': leading_time,
+                   'trailing': trailing_time,
+                   'round_of_16': df_qual})
+
+df.sort_values(by=['group', 'round_of_16', 'leading'], ascending=[True, False, True], inplace=True)
+df.reset_index(inplace=True)
+del df['index']
+print(df)
+
+#alpha = {1: 1, 0: 0.3}
+#minus = [-x for x in df['trailing']]
+#plt.bar(list(range(0,32)), df['trailing'], bottom=minus, color='#ff5050')
+#plt.bar(list(range(0,32)), df['leading'], color='#66ff66')
+#plt.xticks(list(range(0,32)), df['country'], rotation='vertical')
+#plt.ylabel('Minutes')
+#plt.title('Time Spent Leading & Trailing')
 
 plt.show()
